e854cafbe539c18ba35226d066721e261e175407
Goltsov94 <goltsov2pavel@gmail.com>
2023-09-19 21:53:49 +0300

    header

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import styled from "styled-components";
+import {Header} from "./layout/header/Header";
 
 function App() {
     return (
         <div className="App">
-            <Title>Wfdg </Title>
+    <Header></Header>
+
         </div>
     );
 }
