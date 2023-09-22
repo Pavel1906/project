83b8d016c27411333abbf23332dcb48b25644f1f
Goltsov94 <goltsov2pavel@gmail.com>
2023-09-22 13:49:20 +0300

    Work

--- src/layout/section/works/Works.tsx
+++ src/layout/section/works/Works.tsx
@@ -4,6 +4,9 @@ import {SectionTitle} from "../../../components/menu/SectionTitle";
 import {Menu} from "../../../components/menu/Menu";
 import {FlexWrapper} from "../../../components/FlexWrapper";
 import {Work} from "./Work";
+import prog1 from "../../../assets/img/proj1.png"
+import prog2 from "../../../assets/img/proj2.png"
+
 
 
 const workItems = ["All", "landing page", "React", "spa",]
@@ -14,9 +17,10 @@ export const Works = () => {
         <StyledWorks>
             <SectionTitle>My Works</SectionTitle>
             <Menu menuItems={workItems}/>
-            <FlexWrapper>
-                <Work/>
-                <Work/>
+            <FlexWrapper justify={"space-around"}>
+                <Work title={"Social Network"} src={prog1} text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit."}  />
+                <Work title={"Timer"} src={prog2} text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit  ut labore et dolore magna aliqua Ut enim"}  />
+
             </FlexWrapper>
 
         </StyledWorks>
