5c943312f22c7f810f058dbd76878b2c2f4812d4
Goltsov94 <goltsov2pavel@gmail.com>
2023-09-25 14:22:26 +0300

    slogan

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import {Works} from "./layout/section/works/Works";
 import {Skills} from "./layout/skills/Skills";
 import {Testimony} from "./layout/section/testimony/Testimony";
 import {Contact} from "./layout/section/contact/Contact";
+import {Slogan} from "./layout/section/slogan/Slogan";
+import {Footer} from "./layout/footer/Footer";
 
 function App() {
     return (
@@ -17,6 +19,8 @@ function App() {
             <Works/>
             <Testimony/>
             <Contact/>
+            <Slogan/>
+            <Footer/>
         </div>
     );
 }
---
+++ src/layout/footer/Footer.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import styled from "styled-components";
+import {Icon} from "../../components/icon/Icon";
+import {FlexWrapper} from "../../components/FlexWrapper";
+
+export const Footer = () => {
+    return (
+        <StyledFooter>
+            <FlexWrapper direction={"column"} align={"center"} >
+                <Name>Pavel</Name>
+                <SocialList>
+                    <SocialItem>
+                        <SocialLink>
+                            <Icon height={"21px"}  width={"21px"} viewBox={"0 0 21px 21px"} iconId={"instagram"}/>
+                        </SocialLink>
+                    </SocialItem>
+                    <SocialItem>
+                        <SocialLink>
+                            <Icon height={"21px"} width={"21px"} viewBox={"0 0 21px 21px"} iconId={"linkedin"}/>
+                        </SocialLink>
+                    </SocialItem>
+                    <SocialItem>
+                        <SocialLink>
+                            <Icon height={"21px"} width={"21px"} viewBox={"0 0 21px 21px"} iconId={"vk"}/>
+                        </SocialLink>
+                    </SocialItem>
+                    <SocialItem>
+                        <SocialLink>
+                            <Icon height={"21px"} width={"21px"} viewBox={"0 0 21px 21px"} iconId={"vk"}/>
+                        </SocialLink>
+                    </SocialItem>
+
+                </SocialList>
+                <Copygight>© 2023 Goltsov Pavel, All Rights Reserved.</Copygight>
+            </FlexWrapper>
+
+
+        </StyledFooter>
+    )
+        ;
+};
+const StyledFooter = styled.footer`
+background-color: darkolivegreen;
+  min-height: 20vh;
+`
+
+const Name = styled.span`
+
+`
+const SocialList = styled.ul`
+display: flex;
+  gap:30px
+`
+const SocialItem = styled.li`
+    
+
+`
+const SocialLink = styled.a`
+
+`
+const Copygight = styled.footer`
+
+`
