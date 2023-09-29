eda2442eb7db8e0776f08d2143f52f981852bb7d
Goltsov94 <goltsov2pavel@gmail.com>
2023-09-29 15:14:18 +0300

    Container

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from "styled-components";
 import Logo from "../../components/logo/Logo";
 import {Menu} from "../../components/menu/Menu";
+import {Container} from "../../components/Container";
+import {FlexWrapper} from "../../components/FlexWrapper";
 
 
 const items = ["Home", "Skills", "Works", "Testimony", "Contact",]
@@ -10,8 +12,15 @@ const items = ["Home", "Skills", "Works", "Testimony", "Contact",]
 export const Header = () => {
     return (
         <StyledHeader>
-            <Logo/>
-            <Menu menuItems={items}/>
+            <Container>
+                <FlexWrapper justify={"space-between"}  align={"center"}>
+                    <Logo/>
+                    <Menu menuItems={items}/>
+                </FlexWrapper>
+
+
+            </Container>
+
         </StyledHeader>
     );
 };
@@ -19,6 +28,5 @@ export const Header = () => {
 
 const StyledHeader = styled.header`
   background-color: #89de7f;
-  display: flex;
-  justify-content: space-between;
+  
 `
