8f98264a364bc978166c59c6a1de6248263dea01
Goltsov94 <goltsov2pavel@gmail.com>
2023-10-02 20:32:49 +0300

    Main part 2

--- src/layout/section/Main/Main.tsx
+++ src/layout/section/Main/Main.tsx
@@ -2,19 +2,24 @@ import React from 'react';
 import photo from "../../../assets/img/photo.jpg"
 import styled from "styled-components";
 import {FlexWrapper} from "../../../components/FlexWrapper";
+import {Container} from "../../../components/Container";
 
 export const Main = () => {
     return (
         <StyleMain>
-            <FlexWrapper align={"center"} justify={"space-around"}>
-                <div>
-                    <span>Hi There</span>
-                    <Name>I am Pavel Goltsov</Name>
-                    <MainTitle>A Web Developer (junior). </MainTitle>
-                </div>
+            <Container>
+                <FlexWrapper align={"center"} justify={"space-around"}>
+                    <div>
+                        <span>Hi There</span>
+                        <Name>I am Pavel Goltsov</Name>
+                        <MainTitle>A Web Developer (junior). </MainTitle>
+                    </div>
 
-                <Photo src={photo} alt=""/>
-            </FlexWrapper>
+                    <Photo src={photo} alt=""/>
+                </FlexWrapper>
+            </Container>
+
+
         </StyleMain>
     );
 };
@@ -22,7 +27,7 @@ const StyleMain = styled.section`
   min-height: 100vh;
 `
 const Photo = styled.img`
-  width: 350px;  
+  width: 350px;
   height: 430px;
   top: 296px;
   left: 940px;`
